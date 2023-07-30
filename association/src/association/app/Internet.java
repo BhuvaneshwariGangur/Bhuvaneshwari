1ee0b9dba8d343c7c4f7808c14ea063e2cce872d
Bhuvana <bhuvaneshwari.xworkz@gmail.com>
2023-07-30 10:15:50 -0700

    Association

---
+++ association/src/association/app/Internet.java
@@ -0,0 +1,18 @@
+package association.app;
+
+public class Internet {
+
+	public void amount()
+	{
+		System.out.println("Invoking currency in internet ");
+
+	}
+	
+	public void port()
+	{
+		System.out.println("Invoking server in internet");
+
+	}
+
+	
+}
